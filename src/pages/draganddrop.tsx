a8adb67cd9093efa46480950f4fd9f490d552663
notnulldev <notnulldev0@gmail.com>
2022-11-06 00:04:04 +0100

    refactor

--- src/pages/draganddrop.tsx
+++ src/pages/draganddrop.tsx
@@ -9,9 +9,138 @@ type Item = {
 
 // https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/drop_event
 export default function A() {
+  return (
+    <div className="mt-10 flex flex-col items-center">
+      <div className="flex gap-40">
+        <SimpleReorderList />
+        <DragDropNative />
+      </div>
+    </div>
+  );
+}
+
+const DragDropNative = () => {
+  const currentDrag = useRef<HTMLDivElement>(null);
   const ref = useRef(null);
   const isInView = useInView(ref);
-  const currentDrag = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (isInView) {
+      toast("im in view!");
+    } else {
+      toast("im out!!!");
+    }
+  }, [isInView]);
+
+  return (
+    <>
+      <motion.div
+        drag
+        draggable
+        onDragCapture={(e) => {
+          toast("hmm...");
+        }}
+        onDragEnter={() => {
+          toast("drag over");
+        }}
+        // dragConstraints={{ top: 100, left: 100, right: 100, bottom: 100 }}
+        onDragEnd={(e) => {
+          toast("drag end!");
+          console.log(e);
+        }}
+        ref={ref}
+        onDragStart={(e) => {
+          if (e.currentTarget) currentDrag.current = e.currentTarget;
+          toast("drag start");
+        }}
+        whileDrag={{ scale: "1.2" }}
+      >
+        <button className="sm glass btn"> Im free!</button>
+      </motion.div>
+
+      <motion.div className="flex gap-10" layout>
+        <motion.div
+          onDragOver={(e) => {
+            e.preventDefault();
+          }}
+          onDragEnter={() => {
+            toast("drag over");
+          }}
+          onDrop={(e) => {
+            toast("drop!");
+            console.log("drop");
+            console.dir(e);
+            console.log("saved current drag:");
+            console.dir(currentDrag.current);
+            if (currentDrag.current) {
+              currentDrag.current.parentNode?.removeChild(currentDrag.current);
+              e.currentTarget.appendChild(currentDrag.current);
+            }
+          }}
+          className="flex h-[300px] w-[300px] flex-col gap-3 bg-base-200 py-2"
+        >
+          <div
+            draggable
+            className="mx-auto w-3/4 cursor-pointer rounded-xl bg-base-100 p-2 "
+            onDragStart={(e) => {
+              if (e.currentTarget) currentDrag.current = e.currentTarget;
+              toast("drag start");
+            }}
+          >
+            Item 1
+          </div>
+          <div
+            draggable
+            className="mx-auto w-3/4 cursor-pointer rounded-xl bg-base-100 p-2 "
+            onDragStart={(e) => {
+              if (e.currentTarget) currentDrag.current = e.currentTarget;
+              toast("drag start");
+            }}
+          >
+            Item 2
+          </div>
+          <div
+            draggable
+            className="mx-auto w-3/4 cursor-pointer rounded-xl bg-base-100 p-2 "
+            onDragStart={(e) => {
+              if (e.currentTarget) currentDrag.current = e.currentTarget;
+              toast("drag start");
+            }}
+          >
+            Item 3
+          </div>
+        </motion.div>
+
+        <motion.div
+          onDragOver={(e) => {
+            e.preventDefault();
+          }}
+          onDragEnter={(e) => {
+            e.preventDefault();
+            toast("drag over");
+            console.log("drag over");
+            console.dir(e);
+          }}
+          onDrop={(e) => {
+            toast("drop!");
+            console.log("drop");
+            console.dir(e);
+            console.log("saved current drag:");
+            console.dir(currentDrag.current);
+            if (currentDrag.current) {
+              currentDrag.current.parentNode?.removeChild(currentDrag.current);
+              e.currentTarget.appendChild(currentDrag.current);
+            }
+          }}
+          dragListener
+          className="flex h-[300px] w-[300px] flex-col gap-3 bg-base-200 py-2"
+        ></motion.div>
+      </motion.div>
+    </>
+  );
+};
+
+const SimpleReorderList = () => {
   const [items, setItems] = useState<Item[]>([
     {
       id: 1,
@@ -27,145 +156,30 @@ export default function A() {
     },
   ]);
 
-  useEffect(() => {
-    if (isInView) {
-      toast("im in view!");
-    } else {
-      toast("im out!!!");
-    }
-  }, [isInView]);
-
   return (
-    <div className="mt-10 flex flex-col items-center">
-      <div className="flex gap-40">
-        <Reorder.Group
-          values={items}
-          onReorder={setItems}
-          className="flex flex-col gap-2"
-          axis="y"
-        >
-          {items.map((item) => {
-            return (
-              <Reorder.Item
-                key={item.id}
-                value={item}
-                className="flex gap-2 rounded-xl bg-base-200 p-3 shadow shadow-indigo-600"
-              >
-                <div>{item.id}: </div>
-                <div>{item.name}</div>
-              </Reorder.Item>
-            );
-          })}
-        </Reorder.Group>
-        <motion.div
-          drag
-          draggable
-          onDragCapture={(e) => {
-            toast("hmm...");
-          }}
-          onDragEnter={() => {
-            toast("drag over");
-          }}
-          // dragConstraints={{ top: 100, left: 100, right: 100, bottom: 100 }}
-          onDragEnd={(e) => {
-            toast("drag end!");
-            console.log(e);
-          }}
-          ref={ref}
-          onDragStart={(e) => {
-            if (e.currentTarget) currentDrag.current = e.currentTarget;
-            toast("drag start");
-          }}
-        >
-          <button className="sm glass btn"> Im free!</button>
-        </motion.div>
-
-        <div className="flex gap-10">
-          <motion.div
-            onDragOver={(e) => {
-              e.preventDefault();
-            }}
-            onDragEnter={() => {
-              toast("drag over");
-            }}
-            onDrop={(e) => {
-              toast("drop!");
-              console.log("drop");
-              console.dir(e);
-              console.log("saved current drag:");
-              console.dir(currentDrag.current);
-              if (currentDrag.current) {
-                currentDrag.current.parentNode?.removeChild(
-                  currentDrag.current
-                );
-                e.currentTarget.appendChild(currentDrag.current);
-              }
-            }}
-            className="flex h-[300px] w-[300px] flex-col gap-3 bg-base-200 py-2"
-          >
-            <div
-              draggable
-              className="mx-auto w-3/4 cursor-pointer rounded-xl bg-base-100 p-2 "
-              onDragStart={(e) => {
-                if (e.currentTarget) currentDrag.current = e.currentTarget;
-                toast("drag start");
-              }}
+    <>
+      <Reorder.Group
+        values={items}
+        onReorder={setItems}
+        className="flex flex-col gap-2"
+        axis="y"
+      >
+        {items.map((item) => {
+          return (
+            <Reorder.Item
+              key={item.id}
+              value={item}
+              className="flex gap-2 rounded-xl bg-base-200 p-3 shadow shadow-indigo-600"
             >
-              Item 1
-            </div>
-            <div
-              draggable
-              className="mx-auto w-3/4 cursor-pointer rounded-xl bg-base-100 p-2 "
-              onDragStart={(e) => {
-                if (e.currentTarget) currentDrag.current = e.currentTarget;
-                toast("drag start");
-              }}
-            >
-              Item 2
-            </div>
-            <div
-              draggable
-              className="mx-auto w-3/4 cursor-pointer rounded-xl bg-base-100 p-2 "
-              onDragStart={(e) => {
-                if (e.currentTarget) currentDrag.current = e.currentTarget;
-                toast("drag start");
-              }}
-            >
-              Item 3
-            </div>
-          </motion.div>
-
-          <motion.div
-            onDragOver={(e) => {
-              e.preventDefault();
-            }}
-            onDragEnter={(e) => {
-              e.preventDefault();
-              toast("drag over");
-              console.log("drag over");
-              console.dir(e);
-            }}
-            onDrop={(e) => {
-              toast("drop!");
-              console.log("drop");
-              console.dir(e);
-              console.log("saved current drag:");
-              console.dir(currentDrag.current);
-              if (currentDrag.current) {
-                currentDrag.current.parentNode?.removeChild(
-                  currentDrag.current
-                );
-                e.currentTarget.appendChild(currentDrag.current);
-              }
-            }}
-            dragListener
-            className="flex h-[300px] w-[300px] flex-col gap-3 bg-base-200 py-2"
-          ></motion.div>
-        </div>
-      </div>
-    </div>
+              <div>{item.id}: </div>
+              <div>{item.name}</div>
+            </Reorder.Item>
+          );
+        })}
+      </Reorder.Group>
+    </>
   );
-}
+};
 
 const ATable = () => {
   return (
